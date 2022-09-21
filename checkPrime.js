97b416f02a177a05dbd677d3c24bc94059bbb466
prityyad158 <prityyad258@gmail.com>
2022-09-22 01:25:37 +0530

    This is a function  for prime number

--- checkPrime.js
+++ checkPrime.js
@@ -1,9 +1,9 @@
 function checkPrime(num){
   let factors=0;
   for(let i=1;i<=num;i++){
-    if(num%i){
-      factors++;
-    }
+ 	if(num%i==0){
+		factors++;
+	}
   }
   if(factors==2){
     return true;
